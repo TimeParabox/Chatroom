e5e5681d889db2cc774ff6a228c772e7080dd43e
TimeParadoxxx <m18357788331@183.com>
2024-05-01 22:05:01 +0800

    添加项目文件。

---
+++ Client/Client.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    public class Client
+    {
+        private Socket socket = null;
+        private string IP = string.Empty;
+        private int Port = 0;
+        private byte[] buffer = new byte[1024 * 1024 * 2];
+        private Dictionary<string, Socket> clientDic = new Dictionary<string, Socket>();
+        public List<string> UserList = new List<string>();
+
+        public Client(string ip, int port)
+        {
+            this.IP = ip;
+            this.Port = port;
+        }
+
+        public void ClientConnect(int port)
+        {
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress address = IPAddress.Parse(IP);
+                IPEndPoint endPoint = new IPEndPoint(address, Port);
+                socket.Bind(new IPEndPoint(IPAddress.Parse("0.0.0.0"), port));
+                socket.Connect(endPoint);
+                Console.WriteLine("连接服务器成功");
+                Thread MessageThread = new Thread(Message);
+                Thread SendThread = new Thread(Send);
+                MessageThread.Start();
+                SendThread.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void Message()
+        {
+            try
+            {
+                while (true)
+                {
+                    int lengths = socket.Receive(buffer);
+                    string stringFromBytes1 = Encoding.UTF8.GetString(buffer, 0, lengths);
+                    Console.WriteLine(stringFromBytes1);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void Send()
+        {
+            while (true)
+            {
+                try
+                {
+                    string Message = Console.ReadLine();
+                    string sendMessage = string.Format(Message);
+                    socket.Send(Encoding.UTF8.GetBytes(sendMessage));
+                    sendMessage = string.Empty;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+
+    internal class MainProgram
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("请输入服务器ip地址");
+            string ip = Console.ReadLine();
+            Console.WriteLine("请输入服务器端口");
+            int serverpPort = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入用户端口");
+            int userPort = int.Parse(Console.ReadLine());
+            Client client = new Client(ip, serverpPort);
+            client.ClientConnect(userPort);
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    public class TCPServer
+    {
+        private Socket socket = null;
+        private string IP = string.Empty;
+        private int Port = 0;
+        private byte[] buffer = new byte[1024 * 1024 * 2];
+        private Dictionary<string, Socket> clientDic = new Dictionary<string, Socket>();
+        public List<string> UserList = new List<string>();
+
+        public TCPServer(string ip, int port)
+        {
+            this.IP = ip;
+            this.Port = port;
+        }
+
+        public TCPServer(int port)
+        {
+            this.IP = "0.0.0.0";
+            this.Port = port;
+        }
+
+        public void SocketListen()
+        {
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress address = IPAddress.Parse(IP);
+                IPEndPoint endPoint = new IPEndPoint(address, Port);
+                socket.Bind(endPoint);
+                socket.Listen(int.MaxValue);
+                Console.WriteLine("服务器已开启");
+                Thread thread = new Thread(ListenClientConnect);
+                thread.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void ListenClientConnect()
+        {
+            try
+            {
+                while (true)
+                {
+                    Socket clientSocket = socket.Accept();
+                    EndPoint clientipe = clientSocket.RemoteEndPoint;
+                    Console.WriteLine($"接收客户端【{clientipe}】连接...");
+                    string IP = clientipe.ToString();
+                    clientDic.Add(clientipe.ToString(), clientSocket);
+                    Console.WriteLine($"用户【{clientipe}】上线");
+                    UserList.Add(clientipe.ToString());
+                    string Users = string.Empty;
+                    foreach (var item in UserList)
+                    {
+                        Users = item + "," + Users;
+                    }
+                    foreach (var kvp in clientDic)
+                    {
+                        kvp.Value.Send(Encoding.UTF8.GetBytes($"{clientipe},进入聊天"));
+                        kvp.Value.Send(Encoding.UTF8.GetBytes($"Users,{Users}"));
+                    }
+                    Thread thread = new Thread(ReceiveMessage);
+                    thread.Start(clientSocket);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void ReceiveMessage(object socket)
+        {
+            try
+            {
+                Socket clientSocket = socket as Socket;
+                while (true)
+                {
+                    try
+                    {
+                        int length = clientSocket.Receive(buffer);
+                        string stringFromBytes = Encoding.UTF8.GetString(buffer, 0, length);
+
+                        Console.WriteLine($"接收客户端消息:{stringFromBytes}");
+                        EndPoint clientipe = clientSocket.RemoteEndPoint;
+                        foreach (var kvp in clientDic)
+                        {
+                            kvp.Value.Send(Encoding.UTF8.GetBytes($"{clientipe},{stringFromBytes}"));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        EndPoint clientipe = clientSocket.RemoteEndPoint;
+                        clientDic.Remove(clientipe.ToString());
+
+                        clientSocket.Shutdown(SocketShutdown.Both);
+                        clientSocket.Close();
+
+                        Console.WriteLine($"用户【{clientipe}】下线");
+
+                        UserList.Remove(clientipe.ToString());
+                        string Users = string.Empty;
+                        foreach (var item in UserList)
+                        {
+                            Users = Users + "," + item;
+                        }
+                        foreach (var kvp in clientDic)
+                        {
+                            kvp.Value.Send(Encoding.UTF8.GetBytes($"Users,{Users}"));
+                            kvp.Value.Send(Encoding.UTF8.GetBytes($"{clientipe},退出聊天"));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    internal class MainProgram
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("请输入ip地址");
+            string ip = Console.ReadLine();
+            Console.WriteLine("请输入端口");
+            int port = int.Parse(Console.ReadLine());
+            TCPServer server = new TCPServer(ip, port);
+            server.SocketListen();
+        }
+    }
+}
